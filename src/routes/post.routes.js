98d4448b0253b5b20771307419c0446fac4ff6da
miglesiasDev <miglesias.dev@gmail.com>
2024-12-03 22:13:34 -0400

    Creation of create post endpoint with its route

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createPost } from "../controllers/post.controllers.js";
 import validatePost from "../validators/createPost.validators.js";
+import { authenticateToken } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 // endpoints 
-router.post('/createPost', validatePost() ,createPost);
+router.post('/posts', authenticateToken, createPost);
 
 
-export default router;
+export default router; 

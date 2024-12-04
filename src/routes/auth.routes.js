1be6e8be385f28bb6be9583fc9a9dc4ed7a76b45
miglesiasDev <miglesias.dev@gmail.com>
2024-12-03 22:12:51 -0400

    A protected endpoint to check how the middleware works

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,15 +2,21 @@ import { Router } from "express";
 import { login, register } from "../controllers/auth.controllers.js";
 import validateUserLogin from "../validators/login.validators.js";
 import validateUserRegister from "../validators/register.validators.js";
+import { authenticateToken } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 // endpoints 
 router.post('/login', validateUserLogin() ,login);
 
-
-
-
 router.post('/register',validateUserRegister(),register);
+
+// Endpoint de prueba protegido
+router.get('/protected', authenticateToken, (req, res) => {
+    res.json({
+        message: 'Access granted',
+        user: req.user, // Información extraída del token
+    });
+});
 
 export default router;

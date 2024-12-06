97320ffd766b8f237d320cad622db38b4a3e61b8
mirieliz <mirieliz.e@gmail.com>
2024-12-05 21:12:47 -0400

    modified login endpoint

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,8 +1,6 @@
 import { pool } from "../database/connection.database.js";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { uploadImage } from "../helpers/cloudinary.helpers.js";
-import fs from 'fs-extra';
 
 export const login= async (req,res)=>{
     
@@ -11,18 +9,12 @@ export const login= async (req,res)=>{
     const data = req.body;
 
     try {
-        //llamalo rows porque sino va a petar
+        //validar si el correo esta en los registros de la base de datos
         const { rows } = await pool.query( "select * from Users where email = $1",[data.email])
         if( rows.length ===0){
             return res.json({mensaje: "credential not found"})
         }
-        const emailMatch= rows[0].email             //Revisar si hace falta, (parece no ser necesaria)
-        if(data.email!==emailMatch){
-            return res.json({errors: "this" })
-        }
-        // else{
-        //     return res.json({mensaje: "usuario encontrado"})
-        // }
+        
         const hashPassword = rows[0].user_password
         // console.log(hashPassword)
         const validatePassword = await bcrypt.compare(data.password, hashPassword)
@@ -32,9 +24,9 @@ export const login= async (req,res)=>{
         }
 
         //traemos el username del usuario
-        const idUser = rows[0].username
+        const username = rows[0].username
         //asignacion de JWT con los parametros username and email
-        const token = jwt.sign({username:idUser,email:data.email},process.env.SECRET_TOKEN_KEY,{expiresIn: '1d'}) // payload, key ,duracion del token
+        const token = jwt.sign({username:username,email:data.email},process.env.SECRET_TOKEN_KEY,{expiresIn: '1d'}) // payload, key ,duracion del token
         return res.status(201).json({success: true, mensaje: 'user logged successfully',token:token})
 
 
@@ -57,9 +49,9 @@ export const register= async (req,res)=>{
         }
 
         //definimos una constante para verificar la existencia unica de un username
-        const { uniqueUser } = await pool.query( "select * from Users where username = $1",[data.username]) 
+        const checkUsername = await pool.query( "select * from Users where username = $1",[data.username]) 
         //si el username existe en la BD retornar mensaje de que el nombre de usuario ya existe 
-        if(uniqueUser.length !==0){
+        if(checkUsername.rows.length > 0){
             return res.json({mensaje: "This username already exists"})
         }
 

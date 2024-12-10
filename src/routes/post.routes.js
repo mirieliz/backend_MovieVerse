0334d58c84a6c82903db5f0c076b060612353bbe
miglesiasDev <miglesias.dev@gmail.com>
2024-12-10 19:29:33 -0400

    Creation of endpoint to get posts by movie id

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPost, getRecentPosts, getPostById, searchPosts, updatePost, deletePost, createComment, getPostComments, addLike, removeLike, getLikes} from "../controllers/post.controllers.js";
+import { createPost, getRecentPosts, getPostById, searchPosts, updatePost, deletePost, createComment, getPostComments, addLike, removeLike, getLikes, getPostsByMovieId} from "../controllers/post.controllers.js";
 import validatePost from "../validators/createPost.validators.js";
 import { authenticateToken } from "../middleware/auth.middleware.js";
 import validateComment from "../validators/comments.validator.js";
@@ -31,6 +31,8 @@ router.get('/posts/:postId/like', authenticateToken, getLikes);
 router.post('/posts/:postId/like', authenticateToken, addLike);
 
 router.delete('/posts/:postId/like', authenticateToken, removeLike);
+
+router.get('/movies/:movieId/posts', authenticateToken, getPostsByMovieId)
 
 
 
